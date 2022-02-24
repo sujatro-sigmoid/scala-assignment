798bcbdee0e33ea9befc5d436a6e9d3185ad2324
user <sujatrom@sigmoidanalytics.com>
2022-02-24 21:30:36 +0530

    fixes

--- src/main/scala/bucket.scala
+++ src/main/scala/bucket.scala
@@ -1,19 +1,15 @@
 object bucket{
-  def truncateAt(x: Double , p:Int): Double = {
-    val s = math.pow(10, p)
-    (x*s).toInt / s
-  }
-
-  def functionBucketise(num: Double):(Double,Double)={
-    val v0 = (num/0.05).floor * 0.05; // initial value
-    val v1 = (num/0.05).floor * 0.05 + 0.049; // final value
-    return (truncateAt(v0, 3), truncateAt(v1, 3))
+  def getBucket(num: Double): (Double, Double) ={
+    val v0 = (num/0.05).toInt * 0.05; // initial value
+    val v1 = (num/0.05).toInt * 0.05 + 0.049; // final value
+    (BigDecimal(v0).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble,
+      BigDecimal(v1).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble)
   }
 
   def main(args: Array[String]) :Unit={
-    val nums = Array(12.05, 12.03, 10.33, 11.45, 13.50)
+    val nums = Array(12.05, 12.03, 10.33, 11.45, 13.50, 67.90, 999.999, 1234.9999, 00.8992)
     for(n <- nums){
-      println(s"The bucket for value $n is \t ${functionBucketise(n)}")
+      println(s"The bucket for value $n is \t ${getBucket(n)}")
     }
   }
 }
--- src/main/scala/players.scala
+++ src/main/scala/players.scala
@@ -1,30 +1,25 @@
 class players(var year: Int,
-                       var playerName: String,
-                       var country: String,
-                       var matches: Int,
-                       var runs: Int,
-                       var wickets: Int) {}
+              var playerName: String,
+              var country: String,
+              var matches: Int,
+              var runs: Int,
+              var wickets: Int)
 
 object players {
-  def Desc[T: Ordering] = implicitly[Ordering[T]].reverse;
-
-  def push_back(year: Int, playerName: String, country: String, matches: Int, runs: Int, wickets: Int): players = {
-    return new players(year, playerName, country, matches, runs, wickets)
-  }
 
   def main(args: Array[String]): Unit = {
-    var playerList = List(
-      push_back(2010, "Abc", "India", 30, 2000, 99),
-      push_back(2011, "Bcd", "India", 31, 3000, 77),
-      push_back(2012, "Cde", "Pakistan", 32, 4000, 88),
-      push_back(2013, "Efg", "India", 33, 1000, 99),
-      push_back(2014, "Fgh", "Australia", 34, 5000, 88),
-      push_back(2015, "Ijk", "England", 35, 1500, 78),
-      push_back(2016, "Jkl", "Bangladesh", 36, 3000, 67)
+    var playerList = Array(
+      new players(2010, "Abc", "India", 30, 2000, 101),
+      new players(2011, "Bcd", "India", 31, 3000, 77),
+      new players(2012, "Cde", "Pakistan", 32, 4000, 88),
+      new players(2013, "Efg", "India", 33, 1000, 99),
+      new players(2014, "Fgh", "Australia", 34, 5000, 90),
+      new players(2015, "Ijk", "England", 35, 1500, 78),
+      new players(2016, "Jkl", "Bangladesh", 36, 3000, 67)
     )
 
-    println("Player with the best highest run scored");
-    val playersHighestRun = playerList.sortBy(x => x.runs).reverse
+    println("Player with the best highest run scored")
+    val playersHighestRun = playerList.sortBy(obj => obj.runs).reverse
     println(playersHighestRun.head.playerName)
 
     println("Top 5 players by run scored")
@@ -32,17 +27,15 @@ object players {
       println(player.playerName)
 
     println("Top 5 players by wicket taken")
-    val playersHighestWickets = playerList.sortBy(x => x.wickets).reverse
+    val playersHighestWickets = playerList.sortBy(obj => obj.wickets).reverse
     for (player <- playersHighestWickets.take(5)) {
       println(player.playerName)
     }
 
     println("Rank players with overall performance give weight 5x to wicket taken and (5/100)x to run scored")
-    playerList = playerList.sortBy(x => x.wickets * 5).sortBy(x => x.runs * 0.05).reverse
-    var counter: Int = 1
-    for (player <- playerList) {
-      println(s"Rank $counter: " + player.playerName)
-      counter += 1;
+    val playersPerformanceList = playerList.sortBy(obj => obj.wickets*5 + obj.runs*0.05).reverse
+    for (player <- playersPerformanceList) {
+      println(player.playerName)
     }
   }
 }
